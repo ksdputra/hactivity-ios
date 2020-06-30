9a8bcf0d9fdc837bd5525983e6ab0b3b41fb1cbd
Kharisma Putra <ks.dwiputra@gmail.com>
2020-06-30 12:59:31 +0800

    Initial Commit

---
+++ Hactivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hactivity
+//
+//  Created by Kharisma Putra on 30/06/20.
+//  Copyright © 2020 Kharisma Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
