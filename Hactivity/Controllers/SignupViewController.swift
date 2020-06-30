b590dc6d7f7dbc58550bec0813058c841a0151c4
Kharisma Putra <ks.dwiputra@gmail.com>
2020-06-30 16:22:11 +0800

    Updating views; Add controllers

---
+++ Hactivity/Controllers/SignupViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignupViewController.swift
+//  Hactivity
+//
+//  Created by Kharisma Putra on 30/06/20.
+//  Copyright © 2020 Kharisma Putra. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+
+}
